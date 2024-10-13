3944ffd6d015bbcd08cedb8fdc9b76fff9228c1b
ojasvigupta30 <ojasvigupta30@gmail.com>
2024-10-12 23:17:32 -0500

    Added data

--- server.mjs
+++ server.mjs
@@ -1,15 +1,24 @@
 //imports
 import express from 'express';
+import gameRoute from './routes/gameRoute.mjs';
 
 //declaration and initalization
 const app = express();
 const PORT = 3000;
 
 
+//Middleware
+app.use(express.static(`public`));
+
+//Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ extended: true }));
 
 
 
 
+//Routes
+app.use(`/`, gameRoute);
 
 
 
